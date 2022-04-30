573687d77e5c054b5b31e737bd9a1078021bb41d
LJH <snrbs17@gmail.com>
2022-04-30 12:08:44 +0900

    한 줄씩 읽기

--- express2es6.sh
+++ express2es6.sh
@@ -1,1 +1,21 @@
-#!/bin/bash
+#! /bin/bash
+
+if [ $# -eq 1 ]; then
+  path=$1
+fi
+
+if [ ! -d $path/src ]; then
+  mkdir $path/src
+fi
+
+mv $path/{bin,routes,views,app.js} $path/src
+
+cat $path/src/routes/index.js
+
+formattedString=$(python3 ./format.py \"$(cat $path/src/routes/index.js)\")
+
+echo $formattedString
+
+if [ -f $path/bin/www ]; then
+  mv $path/bin/www $path/bin/www.js
+fi
