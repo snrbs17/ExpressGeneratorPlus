21e3a396473c8118eb21aa9fb27cfa907ef7946a
LJH <snrbs17@gmail.com>
2022-05-01 09:26:28 +0900

    export default

--- format.py
+++ format.py
@@ -10,19 +10,25 @@ def formatter(file):
         for line in lines:
             if 'require' in line:
                 fileWriter.write(require2import(line))
+            elif 'module.exports' in line:
+                fileWriter.write(export(line))
             else:
                 fileWriter.write(line)
-
-    return file
 
 
 def require2import(sentence):
     sentence = sentence.split(' ')
     name = sentence[1]
-    source = sentence[3].split("\'")[1]
-    return f"import {name} from '{source}'"
+    source = sentence[-1].split("\'")[1]
+    return f"import {name} from '{source}';\n"
+
+
+def export(sentence):
+    sentence = sentence.split(' ')
+    name = sentence[-1]
+    return f"export default {name}\n"
 
 
 if __name__ == "__main__":
     file = sys.argv[1]
-    print(formatter(file))
+    formatter(file)
