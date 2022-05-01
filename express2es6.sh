dcd36ff2f0ef35228e79fa11be15402fe2d56e86
LJH <snrbs17@gmail.com>
2022-05-01 13:43:47 +0900

    package json scripts 추가

--- express2es6.sh
+++ express2es6.sh
@@ -1,21 +1,52 @@
 #! /bin/bash
 
 if [ $# -eq 1 ]; then
-  path=$1
+  path=$(pwd)/$1
+fi
+if [ $# -eq 0 ]; then
+  path=$(pwd)/target
 fi
 
-if [ ! -d $path/src ]; then
-  mkdir $path/src
-fi
+# if [ ! -d $path/src ]; then
+#   mkdir $path/src
+# fi
 
-mv $path/{bin,routes,views,app.js} $path/src
+# www=$(find -P $path -name www)
+# if [ -f $www ]; then
+#   mv $www $www.js
+# fi
 
-cat $path/src/routes/index.js
+# mv $path/{bin,routes,views,app.js} $path/src
 
-formattedString=$(python3 ./format.py \"$(cat $path/src/routes/index.js)\")
+# files=$(find $path/ -type f \! \( -type d -name node_modules -prune false \) | grep '\.js$')
+# echo 'find .js'
+# echo $files
 
-echo $formattedString
+# for file in $files
+# do
+#   echo $file
+#   python3 ./format.py $file
+# done
 
-if [ -f $path/bin/www ]; then
-  mv $path/bin/www $path/bin/www.js
-fi
+# sed -i '' "s/public/..\/public/" $path/src/app.js
+
+
+# cd $path
+# echo $(pwd)
+
+# npm i --save npm-run-all
+
+# npm install -D @babel/core @babel/cli @babel/preset-env @babel/node
+# echo $path
+# echo "{ \"presets\": [\"@babel\/preset-env\"]}" > "$path/.babelrc"
+
+# sed -i '' "s/scripts/a\ \"start\": \"node \.\/bin\/www\",/" $path/package.json
+sed -i '' "/scripts/ a\\
+\"start\": \"node \.\/bin\/www.js\",
+" $path/package.json
+sed -i '' "/scripts /a\\
+\"server\": \"babel-node \.\/src\/bin\/www\",
+" $path/package.json
+sed -i '' "/scripts /a\\
+\"dev\": \"dev\": \"NODE_ENV=development npm-run-all server\",
+" $path/package.json
