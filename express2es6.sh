7da75cc757b3ba9013ccf3b27d64284f2dafbe0c
LJH <snrbs17@gmail.com>
2022-05-01 13:50:26 +0900

    주석 제거

--- express2es6.sh
+++ express2es6.sh
@@ -7,40 +7,39 @@ if [ $# -eq 0 ]; then
   path=$(pwd)/target
 fi
 
-# if [ ! -d $path/src ]; then
-#   mkdir $path/src
-# fi
+if [ ! -d $path/src ]; then
+  mkdir $path/src
+fi
 
-# www=$(find -P $path -name www)
-# if [ -f $www ]; then
-#   mv $www $www.js
-# fi
+www=$(find -P $path -name www)
+if [ -f $www ]; then
+  mv $www $www.js
+fi
 
-# mv $path/{bin,routes,views,app.js} $path/src
+mv $path/{bin,routes,views,app.js} $path/src
 
-# files=$(find $path/ -type f \! \( -type d -name node_modules -prune false \) | grep '\.js$')
-# echo 'find .js'
-# echo $files
+files=$(find $path/ -type f \! \( -type d -name node_modules -prune false \) | grep '\.js$')
+echo 'find .js'
+echo $files
 
-# for file in $files
-# do
-#   echo $file
-#   python3 ./format.py $file
-# done
+for file in $files
+do
+  echo $file
+  python3 ./format.py $file
+done
 
-# sed -i '' "s/public/..\/public/" $path/src/app.js
+sed -i '' "s/public/..\/public/" $path/src/app.js
 
 
-# cd $path
-# echo $(pwd)
+cd $path
+echo $(pwd)
 
-# npm i --save npm-run-all
+npm i --save npm-run-all
 
-# npm install -D @babel/core @babel/cli @babel/preset-env @babel/node
-# echo $path
-# echo "{ \"presets\": [\"@babel\/preset-env\"]}" > "$path/.babelrc"
+npm install -D @babel/core @babel/cli @babel/preset-env @babel/node
+echo $path
+echo "{ \"presets\": [\"@babel\/preset-env\"]}" > "$path/.babelrc"
 
-# sed -i '' "s/scripts/a\ \"start\": \"node \.\/bin\/www\",/" $path/package.json
 sed -i '' "/scripts/ a\\
 \"start\": \"node \.\/bin\/www.js\",
 " $path/package.json
